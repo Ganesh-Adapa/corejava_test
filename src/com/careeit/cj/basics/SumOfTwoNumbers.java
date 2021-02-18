12d9beb5a18177eac553f39083afb32e3621fa7d
unknown <ganesh@technocentra.com>
2021-02-18 11:48:32 +0530

    Added comment for test

--- src/com/careeit/cj/basics/SumOfTwoNumbers.java
+++ src/com/careeit/cj/basics/SumOfTwoNumbers.java
@@ -9,6 +9,7 @@ public class SumOfTwoNumbers {
 		System.out.println("The Sum Of " + num1 + " + " + num2 + " = " +sum);
 		//System.out.printf("The Sum of %d + %d = %d", num1,num2,sum);
 		//System.out.println(String.format("The Sum of %d + %d = %d", num1,num2,sum));
+		//test commit
 	}
 
 }
