723fd2006f5bb483b56db4a4f1f362b39df64a36
unknown <ganesh@technocentra.com>
2021-02-18 11:39:47 +0530

    First git java project

---
+++ src/com/careeit/cj/basics/SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.careeit.cj.basics;
+
+public class SumOfTwoNumbers {
+	
+	public static void main(String[] args) {
+		int num1 =10;
+		int num2 = 20;
+		int sum = num1 + num2;
+		System.out.println("The Sum Of " + num1 + " + " + num2 + " = " +sum);
+		//System.out.printf("The Sum of %d + %d = %d", num1,num2,sum);
+		//System.out.println(String.format("The Sum of %d + %d = %d", num1,num2,sum));
+	}
+
+}
